57409ca813ee83d5524e6dc58b495470d8c13f1c
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-06-10 10:46:15 +0300

    preservering state

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,4 @@
-import {defineStore} from "pinia";
+import {defineStore, acceptHMRUpdate} from "pinia";
 import {groupBy} from "lodash";
 import {useAuthUserStore} from "@/stores/AuthUserStore";
 
@@ -48,3 +48,7 @@ export const useCartStore = defineStore ('CartStore', {
     },
 
 })
+
+if (import.meta.hot) {
+    import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot))
+}
