038d5053efb6c5aeeebfe2f7d0d229479be01d3a
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-06-10 11:27:49 +0300

    subscribing to state to handle undo

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,25 @@ import ProductCard from "@/components/ProductCard.vue";
 // import products from "@/data/products.json";
 import {useProductStore} from "@/stores/ProductStore";
 import {useCartStore} from "@/stores/CartStore";
+import {ref, reactive} from "vue";
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
+const history = reactive([])
+const doingHistory = ref(false)
+history.push(JSON.stringify(cartStore.$state))
+const undo = () => {
+  if (history.length === 1) return
+    doingHistory.value = true
+    history.pop()
+    cartStore.$state = JSON.parse(history.at(-1))
+    doingHistory.value = false
+}
+cartStore.$subscribe((mutation, state) => {
+  if (!doingHistory.value) {
+    history.push(JSON.stringify(state))
+  }
+})
 cartStore.$onAction(({
   name,
   store,
@@ -32,6 +48,9 @@ productStore.fill()
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+    <AppButton @click="undo">Undo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
